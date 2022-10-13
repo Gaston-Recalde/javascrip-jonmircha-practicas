c7f760ab655c32fd923bc07bf980790837205b6f
Gaston Recalde <gastonrg94@gmail.com>
2022-10-13 09:33:38 -0300

    Ejercicio DOM - Cuenta Regresiva

--- ejercicios-dom/js/app.js
+++ ejercicios-dom/js/app.js
@@ -1,6 +1,7 @@
 import hamburgerMenu from "./menu_hamburguesa.js";
 import { digitalClock, alarm } from "./reloj.js";
 import { shortcuts, moveBall } from "./teclado.js";
+import countdown from "./cuenta_regresiva.js";
 
 const d = document;
 
@@ -8,6 +9,7 @@ d.addEventListener('DOMContentLoaded', (e) => {
     hamburgerMenu('.panel-btn', '.panel', '.menu a');
     digitalClock('#reloj', '#activar-reloj', '#desactivar-reloj');
     alarm('assets/alarma.mp3', '#activar-alarma', '#desactivar-alarma');
+    countdown('countdown', 'Jun 27, 2023 09:15:00', 'Feliz Cumpleaños!!!');
 });
 
 d.addEventListener('keydown', (e) => {
