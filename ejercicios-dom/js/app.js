4524da063bfe7a7cf844f6fa2ea0c55257e4bd77
Gaston Recalde <gastonrg94@gmail.com>
2022-10-13 11:04:31 -0300

    Ejercicio DOM - Dark Mode

--- ejercicios-dom/js/app.js
+++ ejercicios-dom/js/app.js
@@ -3,6 +3,7 @@ import { digitalClock, alarm } from "./reloj.js";
 import { shortcuts, moveBall } from "./teclado.js";
 import countdown from "./cuenta_regresiva.js";
 import scrollTopButton from "./boton_scroll.js";
+import darkTheme from "./tema_oscuro.js";
 
 const d = document;
 
@@ -12,6 +13,7 @@ d.addEventListener('DOMContentLoaded', (e) => {
     alarm('assets/alarma.mp3', '#activar-alarma', '#desactivar-alarma');
     countdown('countdown', 'Jun 27, 2023 09:15:00', 'Feliz Cumpleaños!!!');
     scrollTopButton('.scroll-top-btn');
+    darkTheme('.dark-theme-btn', 'dark-mode');
 });
 
 d.addEventListener('keydown', (e) => {
