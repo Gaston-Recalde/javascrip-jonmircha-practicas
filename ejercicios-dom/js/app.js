ae96b9cd4393ebd4973e56a52e156810e1b4c3b9
Gaston Recalde <gastonrg94@gmail.com>
2022-10-14 14:45:04 -0300

    Ejercicio DOM - Detección WebCam

--- ejercicios-dom/js/app.js
+++ ejercicios-dom/js/app.js
@@ -8,6 +8,7 @@ import responsiveMedia from "./objeto_responsive.js";
 import responsiveTester from "./prueba_responsive.js";
 import userDeviceInfo from "./deteccion_dispositivos.js";
 import networkStatus from "./deteccion_red.js";
+import webCam from "./deteccion_webcan.js";
 
 const d = document;
 
@@ -21,6 +22,7 @@ d.addEventListener('DOMContentLoaded', (e) => {
     responsiveMedia('gmaps', '(min-width: 768px)', `<a href='https://goo.gl/maps/M2oiDF8tewSgTfjk6'>Ver Mapa</a>`, `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2624.9914406082157!2d2.29229261588788!3d48.85837360866145!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e66e2964e34e2d%3A0x8ddca9ee380ef7e0!2sTorre%20Eiffel!5e0!3m2!1ses-419!2suy!4v1665685357029!5m2!1ses-419!2suy" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`);
     responsiveTester('responsive-tester');
     userDeviceInfo('user-device');
+    webCam('webcam');
 });
 
 d.addEventListener('keydown', (e) => {
